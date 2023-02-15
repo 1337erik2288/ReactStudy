dd02f89724cb262e02f1d1a114356c76dfa05bfc
Koncheny_FNAFer228 <Koncheny_FNAFer228@LAPTOP-BN9SOC5T>
2023-02-15 14:01:34 +0800

    41:58

--- src/App.js
+++ src/App.js
@@ -3,20 +3,18 @@ import Counter from "./components/Counter";
 import ClassCounter from "./components/ClassCounter";
 import './styles/App.css';
 import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
 
 
 function App() {
     const [posts, setPosts] = useState([
         {id: 1, title: 'Javascript', body: 'Description'},
         {id: 2, title: 'Javascript 2', body: 'Description'},
-        {id: 3, title: 'Javascript 3', body: 'Description'},
     ])
 
     return (
     <div className="App">
-        {posts.map(post =>
-            <PostItem post={post}/>
-        )}
+        <PostList posts={posts} title="Список приколов 1"/>
     </div>
   );
 }
