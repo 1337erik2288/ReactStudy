cf6a1022f5b97317788d999a02b46c23e32ca022
Koncheny_FNAFer228 <Koncheny_FNAFer228@LAPTOP-BN9SOC5T>
2023-02-25 19:56:43 +0800

    КАК ЖЕ ЖЕСКА Я СОРТИРУЮ, А ЕБАНЫЙ ГИТХАБ МЕНЯ ИГНОРИТ ААААААААА1111!!!!!!1!11!111111!

--- src/App.js
+++ src/App.js
@@ -7,13 +7,16 @@ import PostList from "./components/PostList";
 import MyButton from "./components/UI/button/MyButton";
 import MyInput from "./components/UI/input/MyInput";
 import PostForm from "./components/PostForm";
+import MySelect from "./components/UI/select/MySelect";
 
 
 function App() {
     const [posts, setPosts] = useState([
         {id: 1, title: 'Javascript', body: 'Description'},
-        {id: 2, title: 'React', body: 'Description'},
+        {id: 2, title: 'React', body: 'scription'},
+        {id: 3, title: 'Pascal😶', body: 'prikol'},
     ])
+    const [selectedSort, setSelectedSort] = useState('')
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
@@ -23,15 +26,25 @@ function App() {
         setPosts(posts.filter(p => p.id !== post.id))
     }
 
+    const sortPosts = (sort) => {
+        setSelectedSort(sort);
+        setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])))
+    }
+
     return (
     <div className="App">
         <PostForm create={createPost}/>
         <hr style={{margin: '15px 0'}}/>
         <div>
-            <select>
-                <option value="value1">По названию</option>
-                <option value="value1">По описанию</option>
-            </select>
+            <MySelect
+                value={selectedSort}
+                onChange={sortPosts}
+                defaultValue="Сортировка"
+                options={[
+                    {value: 'title', name: 'По названию'},
+                    {value: 'title', name: 'По описанию'}
+                ]}
+            />
         </div>
         {posts.length !== 0
             ?
