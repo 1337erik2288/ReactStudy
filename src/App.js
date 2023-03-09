c26c7b470f4b5d438ddca6d7b633b3f41575b984
Koncheny_FNAFer228 <Koncheny_FNAFer228@LAPTOP-BN9SOC5T>
2023-03-09 13:49:24 +0800

    Подцепляем посты при прогрузке страницы

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useMemo, useRef, useState} from 'react';
+import React, {useEffect, useMemo, useRef, useState} from 'react';
 import Counter from "./components/Counter";
 import ClassCounter from "./components/ClassCounter";
 import './styles/App.css';
@@ -11,21 +11,27 @@ import MySelect from "./components/UI/select/MySelect";
 import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/MyModal/MyModal";
 import {usePosts} from "./hooks/usePosts";
+import axios from "axios";
 
 
 function App() {
-    const [posts, setPosts] = useState([
-        {id: 1, title: 'Javascript', body: 'Description'},
-        {id: 2, title: 'React', body: 'scription'},
-        {id: 3, title: 'Pascal😶', body: 'prikol'},
-    ])
+    const [posts, setPosts] = useState([])
     const [filter, setFilter] = useState({sort: '', query: ''})
     const [modal, setModal] = useState(false);
     const sortedAndSearchedPosts = usePosts(posts,filter.sort, filter.query);
 
+    useEffect(() => {
+        fetchPosts()
+    }, [])
+
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
         setModal(false)
+    }
+
+    async function fetchPosts() {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
+        setPosts(response.data)
     }
 
     const removePost = (post) => {
@@ -34,6 +40,7 @@ function App() {
 
     return (
     <div className="App">
+        <button onClick={fetchPosts}>GET POSTS</button>
         <MyButton style={{marginTop: 20}} onClick={() => setModal(true)}>
             Создать новый пост
         </MyButton>
